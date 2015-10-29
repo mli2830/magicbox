6c1a2fc77fb39699272276ebe381997606668b67
mikelin2 <wzmichael.li@gmail.com>
2015-10-28 23:42:03 -0400

    it works but there are problems

--- miketest.R
+++ miketest.R
@@ -1,37 +1,43 @@
 library(R2jags)
 
-simsirdata <- function(seed=1000,
-                          beta=0.5,gamma=0.5,s0=49,i0=1,t0=1,
-                       tot.t=10,dt=1,N=50,Pc=0.5) {
+
+simdat <- function(seed=1099,beta=0.5,gamma=0.5,s0=490,i0=10,t0=1,
+                       tot.t=20,dt=1,N=500,Pc=0.5) {
   if (!is.null(seed)) set.seed(seed)
   tvec <- seq(1,tot.t,by=dt)
   n <- length(tvec)
   S <- numeric(n)
   I <- numeric(n)
+  Pi <- numeric(n)
+  inf <- numeric(n)
+  rec <- numeric(n)
   Iobs <- numeric(n)
   S[1] <- s0
   I[1] <- i0
   Pr <- 1 - exp(-gamma*dt)
-  Pi <- 1 - exp(-beta*I[1]*dt/N)
-  inf <- rbinom(1,S[1],Pi)
-  rec <- rbinom(1,I[1],Pr)
+  Pi[1] <- 1 - exp(-beta*I[1]*dt/N)
+  inf[1] <- rbinom(1,S[1],Pi[1])
+  rec[1] <- rbinom(1,I[1],Pr)
   Iobs[1] <- rbinom(1,I[1],Pc)
   for (i in 2:n) {
-    S[i] <- S[i-1] - inf
-    I[i] <- I[i-1] - rec + inf
+    S[i] <- S[i-1] - inf[i-1]
+    I[i] <- I[i-1] - rec[i-1] + inf[i-1]
     Iobs[i] <- rbinom(1,I[i],Pc)
-    Pi <- 1 - exp(-beta*I[i]*dt/N)
-    inf <- rbinom(1,S[i],Pi)
-    rec <- rbinom(1,I[i],Pr)
+    Pi[i] <- 1 - exp(-beta*I[i]*dt/N)
+    inf[i] <- rbinom(1,S[i],Pi)
+    rec[i] <- rbinom(1,I[i],Pr)
   }
-  cbind(tvec,Iobs)
+  cbind(tvec,S,I,Iobs)
 }
 
-sirdat <- simsirdata(tot.t=20,beta=0.6,gamma=0.1,Pc=0.5)
+sirdat <- simdat(seed=13011,tot.t=20,beta=0.4,gamma=0.4,Pc=0.3)
+
+plot(Iobs~tvec,data=sirdat)
+
 
 parameters <- c("beta","gamma","Pc")
 
-jagsdata <- list(o=sirdat[,2], dt=1,M=nrow(sirdat),s0=49, i0=1, N=50)
+jagsdata <- list(o=sirdat[,4], dt=1,M=nrow(sirdat),s0=480, i0=20, N=500)
 
 jags.sir.model <- function() {
     ## initial values
@@ -54,20 +60,19 @@ jags.sir.model <- function() {
     ## aux variables
     
     ## priors
-    beta ~ dunif(0,1)
-    gamma ~ dunif(0,1)
-    Pc ~ dunif(0,1)
+    beta ~ dbeta(1,1)
+    gamma ~ dbeta(1,1)
+    Pc ~ dbeta(1,1)
 }
 
-inits <- list(list(beta=0.6,gamma=0.1,Pc=0.5))
+inits <- list(list(beta=0.8,gamma=0.4,Pc=0.5))
 
-j1 <- jags(data=jagsdata,inits, param=parameters,
-           model.file = jags.sir.model,  ## in your working directory 
-           n.chains=length(inits), ##how many chains you want(in the list)
-           n.iter=15000,
-           n.burnin=5000,
-           n.thin=37)
+jagsdata$o
 
+j1 <- jags(data=jagsdata,inits, param=parameters, model.file = jags.sir.model,   
+           n.chains=length(inits), n.iter=15000, n.burnin=5000, n.thin=37)
+
+j1
 
 
 
